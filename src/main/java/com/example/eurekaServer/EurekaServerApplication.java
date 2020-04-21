70ca84fa2be1915d5aa200767ff03ac683fa94ab
dixon <vnodavid@gmail.com>
2020-04-21 20:18:14 +0530

    commit

--- src/main/java/com/example/eurekaServer/EurekaServerApplication.java
+++ src/main/java/com/example/eurekaServer/EurekaServerApplication.java
@@ -10,7 +10,7 @@ public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
-		System.out.println("");
+		System.out.println("Test");
 	}
 
 }
